51d506637a116d399f824a14c901ee526a481f0d
KAdamek <karel.adamek@oerc.ox.ac.uk>
2018-05-25 06:30:35 +0100

    Faster way of getting nsamp
    Former-commit-id: 43e4a6bdabf21e85ecb31a7b9da0fa8711186e20

--- lib/host_get_file_data.cu
+++ lib/host_get_file_data.cu
@@ -1,10 +1,13 @@
 #include <stdio.h>
 
+#include "timer.h"
+
 /* Note we send in a pointer to the file pointer becuase this function needs to update the position of the file pointer
  */
 
 void get_file_data(FILE **fp, int *nchans, int *nsamples, int *nsamp, int *nifs, int *nbits, float *tsamp, float *tstart, float *fch1, float *foff)
 {
+	GpuTimer timer;
 
 	fpos_t file_loc;
 
@@ -124,10 +127,28 @@ void get_file_data(FILE **fp, int *nchans, int *nsamples, int *nsamp, int *nifs,
 	}
 
 	fgetpos(*fp, &file_loc);
+	
+	//-----------------------------------------------------------------
+	//Testing different way of getting number of samples
+	timer.Start();
+	size_t data_start = ftell(*fp);
+	if (fseek(*fp, 0, SEEK_END) != 0) {
+		printf("\nERROR!! Failed to seek to end of data file\n");
+		exit(1);
+	}
+	size_t exp_total_data = ftell(*fp);
+	if (exp_total_data == -1) {
+		printf("\nERROR!! Failed to seek to end of data file\n");
+		exit(1);
+	}
+	exp_total_data = exp_total_data - data_start;
+	fseek(*fp, data_start, SEEK_SET);
+	timer.Stop();
+	printf("\nAlternative way took: %f ms;\n", timer.Elapsed());
+	//-----------------------------------------------------------------<
 
-	if (( *nbits ) == 32)
-	{
-		// Allocate a tempory buffer to store a line of frequency data
+	if (( *nbits ) == 32) {
+		// Allocate a temporary buffer to store a line of frequency data
 		float *temp_buffer = (float *) malloc(( *nchans ) * sizeof(float));
 
 		// Count how many time samples we have
@@ -139,6 +160,8 @@ void get_file_data(FILE **fp, int *nchans, int *nsamples, int *nsamp, int *nifs,
 		}
 		*nsamp = total_data - 1;
 		free(temp_buffer);
+		printf("exp_total_data: %zu; nchans: %zu;\n", exp_total_data, *nchans);
+		printf("For 32bit case nsamp=%zu, while faster method gives nsamp=%zu\n", *nsamp, exp_total_data/((*nchans)*4));
 	}
 	else if (( *nbits ) == 16)
 	{
@@ -157,9 +180,11 @@ void get_file_data(FILE **fp, int *nchans, int *nsamples, int *nsamp, int *nifs,
 		}
 		*nsamp = total_data - 1;
 		free(temp_buffer);
+		printf("exp_total_data: %zu; nchans: %zu;\n", exp_total_data, *nchans);
+		printf("For 16bit case nsamp=%zu, while faster method gives nsamp=%zu\n", *nsamp, exp_total_data/((*nchans)*2));
 	}
-	else if (( *nbits ) == 8)
-	{
+	else if (( *nbits ) == 8) {
+		timer.Start();
 		// Allocate a tempory buffer to store a line of frequency data
 		unsigned char *temp_buffer = (unsigned char *) malloc(( *nchans ) * sizeof(unsigned char));
 
@@ -175,6 +200,11 @@ void get_file_data(FILE **fp, int *nchans, int *nsamples, int *nsamp, int *nifs,
 		}
 		*nsamp = total_data - 1;
 		free(temp_buffer);
+		printf("exp_total_data: %zu; nchans: %zu;\n", exp_total_data, *nchans);
+		printf("For 8bit case nsamp=%zu, while faster method gives nsamp=%zu\n", *nsamp, exp_total_data/((*nchans)*1));
+		timer.Stop();
+		printf("Getting number of time samples took: %f ms;\n", timer.Elapsed());
+		*nsamp = exp_total_data/((*nchans)*1);
 	}
 	else if (( *nbits ) == 4)
 	{
