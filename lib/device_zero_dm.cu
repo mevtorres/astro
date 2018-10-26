ed6d397c56f50f854183c4189e03d8800b0482e8
jan2nov <jan.novotny@oerc.ox.ac.uk>
2018-10-26 11:44:54 +0100

    Optimizing zero_dm_kernel; introducing a cache/warp summing; now the number of blocks is the number of nsamp

--- lib/device_zero_dm.cu
+++ lib/device_zero_dm.cu
@@ -9,15 +9,15 @@
 
 void zero_dm(unsigned short *d_input, int nchans, int nsamp, int nbits) {
 
-	int divisions_in_t  = CT;
-	int num_blocks_t    = nsamp/divisions_in_t;
+	int sum_threads     = 256;
+	int num_blocks_t    = nsamp;
 
 	printf("\nCORNER TURN!");
-	printf("\n%d %d", nsamp, nchans);
-	printf("\n%d %d", divisions_in_t, 1);
+	printf("\n%d %d %d", nsamp, nchans, sum_threads);
+	printf("\n%d %d", CT, 1);
 	printf("\n%d %d", num_blocks_t, 1);
 
-	dim3 threads_per_block(divisions_in_t, 1);
+	dim3 threads_per_block(sum_threads, 1);
 	dim3 num_blocks(num_blocks_t,1);
 
 	clock_t start_t, end_t;
@@ -25,7 +25,7 @@ void zero_dm(unsigned short *d_input, int nchans, int nsamp, int nbits) {
 
 	float normalization_factor = ((pow(2,nbits)-1)/2);
 
-	zero_dm_kernel<<< num_blocks, threads_per_block >>>(d_input, nchans, nsamp, normalization_factor);
+	zero_dm_kernel<<< num_blocks, threads_per_block, sum_threads*4 >>>(d_input, nchans, nsamp, normalization_factor);
 	cudaDeviceSynchronize();
 
 	end_t = clock();
--- lib/device_zero_dm_kernel.cu
+++ lib/device_zero_dm_kernel.cu
@@ -5,19 +5,60 @@
 #include <cuda_runtime.h>
 #include "headers/params.h"
 
+__device__ __inline__ void Reduce_SM(float *s_input){
 
-//{{{ zero dm kernel - needs cleaning and optimizing // WA 21/10/16
+        for (int i = ( blockDim.x >> 1 ); i > 16; i = i >> 1) {
+                if (threadIdx.x < i) {
+                        s_input[threadIdx.x] = s_input[threadIdx.x] + s_input[threadIdx.x + i];
+                }
+                __syncthreads();
+        }
+}
+
+__device__ __inline__ void Reduce_WARP(float *sum){
+        for (int q = 16; q > 0; q = q >> 1) {
+                *sum += __shfl_down((*sum), q);
+        }
+}
+
+//{{{ zero dm kernel - needs optimizing? // JN 26/10/18
 __global__ void zero_dm_kernel(unsigned short *d_input, int nchans, int nsamp, float normalization_factor)
 {
 
-	int t  = blockIdx.x * blockDim.x + threadIdx.x;
+        int t  = threadIdx.x;
 
-	float sum = 0.0f;
-	for(int c = 0; c < nchans; c++) sum+=(float)__ldg(&d_input[t*nchans + c]);
-	sum = (sum/(float)nchans-normalization_factor);
-	for(int c = 0; c < nchans; c++) d_input[t*nchans + c]=(unsigned short)((unsigned char)((float)d_input[t*nchans + c]-sum));
+        extern __shared__ float s_input[];
+        float sum = 0.0f;
+
+        int n_iterations = (nchans+blockDim.x-1)/blockDim.x;
+
+        for(int c = 0; c < n_iterations; c++){
+                if ((c*blockDim.x + t) < nchans) {
+                        sum += d_input[blockIdx.x*nchans + c*blockDim.x + t];
+                }
+        }
+
+        s_input[t] = sum;
+        __syncthreads();
+
+        Reduce_SM(s_input);
+        sum = s_input[t];
+
+        Reduce_WARP(&sum);
+
+        if (t  == 0) s_input[0] = sum;
+        __syncthreads();
+        sum = s_input[0];
+
+        sum = (sum/(float)nchans-normalization_factor);
+
+        for(int c = 0; c < n_iterations; c++){
+                if ((c*blockDim.x + t) < nchans) {
+                        d_input[blockIdx.x*nchans + c*blockDim.x + t] =(unsigned short)((unsigned char)((float)d_input[blockIdx.x*nchans + c*blockDim.x + t] - sum));
+                }
+        }
+
 }
-
 //}}}
 
 #endif
